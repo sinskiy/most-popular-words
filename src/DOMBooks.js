0abccdc22a5fc4b6631ca307652e131c4537f956
sinskiy <sinskiy@proton.me>
2024-08-04 12:19:46 +0300

    refactor: move book creation to its own function

--- src/DOMBooks.js
+++ src/DOMBooks.js
@@ -5,19 +5,24 @@ export default function displayBooks() {
   const container = document.createElement("section");
   container.classList.add("books");
   for (const book of books) {
-    const bookContainer = document.createElement("button");
-    bookContainer.classList.add("book");
-
-    const title = document.createElement("h2");
-    title.textContent = book.name;
-
-    const author = document.createElement("p");
-    author.textContent = book.author;
-
-    bookContainer.append(title, author);
+    const bookContainer = createBook(book);
 
     container.appendChild(bookContainer);
   }
   main.innerHTML = "";
   main.appendChild(container);
+}
+
+function createBook(book) {
+  const bookContainer = document.createElement("button");
+  bookContainer.classList.add("book");
+
+  const title = document.createElement("h2");
+  title.textContent = book.name;
+
+  const author = document.createElement("p");
+  author.textContent = book.author;
+
+  bookContainer.append(title, author);
+  return bookContainer;
 }
