fd638f5ec81724749afd617eb089120e1d788cfa
sinskiy <sinskiy@proton.me>
2025-01-02 21:17:39 +0300

    feat: update details from /learn

--- components/learn-word.tsx
+++ components/learn-word.tsx
@@ -7,6 +7,8 @@ import InputField from "../ui/input-field";
 import { updateKnowledge } from "../actions/update-knowledge";
 import Form from "../ui/form";
 import { User } from "../types/user";
+import Tip from "./tip";
+import { setWordDetails } from "../actions/word-details";
 
 interface LearnWordProps {
   words: SavedWord[];
@@ -68,6 +70,8 @@ export default function LearnWord({ user, words, reverse }: LearnWordProps) {
 
   const [showAnswers, setShowAnswers] = useState(false);
 
+  const [isSuccessOld, setIsSuccessOld] = useState(false);
+
   const [state, action, pending] = useActionState(
     updateKnowledge.bind(null, { words: localWords, user: user }),
     undefined
@@ -82,52 +86,14 @@ export default function LearnWord({ user, words, reverse }: LearnWordProps) {
               <i>loading...</i>
             </p>
           ) : (
-            <>
-              {reverse === "true" ? (
-                <ul>
-                  {(["translation", "definition", "example"] as const).map(
-                    (value) => (
-                      <li key={value} className="grid">
-                        <span className="text-sm font-medium text-stone-300">
-                          {value}
-                        </span>
-                        <span className="text-lg">
-                          {filteredWords[randomWordIndex][value] ?? (
-                            <i>no {value}</i>
-                          )}
-                        </span>
-                      </li>
-                    )
-                  )}
-                </ul>
-              ) : (
-                <h1 className="text-4xl font-bold">
-                  {filteredWords[randomWordIndex].value}
-                </h1>
-              )}
-              <div
-                className={cn([
-                  "grid gap-4 w-fit items-center",
-                  showAnswers && "grid-cols-2",
-                ])}
-              >
-                {(reverse === "true"
-                  ? (["value"] as const)
-                  : (["translation", "definition", "example"] as const)
-                ).map((value) => (
-                  <Fragment key={value}>
-                    <InputField type="text" id={value} />
-                    {showAnswers && (
-                      <p>
-                        {filteredWords[randomWordIndex][value] ?? (
-                          <i>no {value}</i>
-                        )}
-                      </p>
-                    )}
-                  </Fragment>
-                ))}
-              </div>
-            </>
+            <LearnWordLoaded
+              word={filteredWords[randomWordIndex]}
+              reverse={reverse}
+              showAnswers={showAnswers}
+              username={user.username}
+              isSuccessOld={isSuccessOld}
+              setIsSuccessOld={setIsSuccessOld}
+            />
           )}
         </>
       ) : (
@@ -143,7 +109,7 @@ export default function LearnWord({ user, words, reverse }: LearnWordProps) {
         >
           show answers
         </button>
-        <div className="flex gap-1 mt-2">
+        <div className="flex gap-1 mt-2" onClick={() => setIsSuccessOld(true)}>
           {knowledge.map((value, i) => (
             <button
               key={value}
@@ -163,6 +129,83 @@ export default function LearnWord({ user, words, reverse }: LearnWordProps) {
         label="save and exit"
       ></Form>
     </main>
+  );
+}
+
+function LearnWordLoaded({
+  reverse,
+  word,
+  showAnswers,
+  username,
+  isSuccessOld,
+  setIsSuccessOld,
+}: {
+  reverse: string;
+  word: SavedWord;
+  showAnswers: boolean;
+  username: string;
+  isSuccessOld: boolean;
+  setIsSuccessOld: (value: boolean) => void;
+}) {
+  const [state, action, pending] = useActionState(
+    setWordDetails.bind(null, {
+      username,
+      word: word.value,
+    }),
+    undefined
+  );
+
+  return (
+    <>
+      {reverse === "true" &&
+        (!word.translation || !word.definition || !word.example) && (
+          <Tip>
+            toggle off <i>reverse learn mode</i> to add translation, definition
+            or example
+          </Tip>
+        )}
+      {reverse === "true" ? (
+        <ul>
+          {(["translation", "definition", "example"] as const).map((value) => (
+            <li key={value} className="grid">
+              <span className="text-sm font-medium text-stone-300">
+                {value}
+              </span>
+              <span className="text-lg">
+                {word[value] ?? <i>no {value}</i>}
+              </span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <h1 className="text-4xl font-bold">{word.value}</h1>
+      )}
+      <Form
+        pending={(state?.success === true && isSuccessOld === false) || pending}
+        action={action}
+        message={state?.message}
+        className={cn([
+          "grid gap-4 w-fit items-center",
+          showAnswers && "grid-cols-2",
+        ])}
+        label="update details"
+        showSubmit={reverse !== "true"}
+        onSubmit={() => setIsSuccessOld(false)}
+      >
+        {(reverse === "true"
+          ? (["value"] as const)
+          : (["translation", "definition", "example"] as const)
+        ).map((value) => (
+          <Fragment key={value}>
+            <InputField type="text" id={value} />
+            {showAnswers && <p>{word[value] ?? <i>no {value}</i>}</p>}
+          </Fragment>
+        ))}
+      </Form>
+      {state?.success === true && isSuccessOld === false && (
+        <p>successfully updated details</p>
+      )}
+    </>
   );
 }
 
