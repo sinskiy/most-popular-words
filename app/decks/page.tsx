f51eefcf07379fca7030d50665961e3f032ad0a5
sinskiy <sinskiy@proton.me>
2025-01-11 16:42:09 +0300

    feat: add deck dialog

--- app/decks/page.tsx
+++ app/decks/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <button className="button">add deck</button>;
+import { getSaved } from "../../actions/saved";
+import { getUser } from "../../actions/auth";
+import ErrorPage from "../../components/error-page";
+import AddDeck from "../../components/add-deck";
+
+export default async function Page() {
+  const user = await getUser();
+  if (!user) {
+    return <ErrorPage title={401}>Unauthorized</ErrorPage>;
+  }
+  const savedWordsQuery = await getSaved(0, user?.username);
+  return <AddDeck words={savedWordsQuery.rows} />;
 }
